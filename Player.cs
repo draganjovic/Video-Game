f9a49473bdd7a1d34bded6b62e21e979feb8d29a
Dragan Jović <dj47@zips.uakron.edu>
2016-04-20 12:17:37 -0400

    Video Game
    Coded in C#

---
+++ Player.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace Oops.Game
+{
+	public class Player
+	{
+		public object Weapon { get; set; }
+		public Armor Armor { get; set; }
+
+		Player()
+		{
+
+			Weapon = new WoodenSword ();
+		}
+
+	}
+}
+
+// Things we need for a game: Player/stats, Badguys, map/levels, items, Gold, input
