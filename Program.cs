6b93c4027c4e187634b6488ae22c86b8c1493041
draganjovic <dj47@zips.uakron.edu>
2017-02-14 02:10:00 -0500

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -6,14 +6,8 @@ namespace Oops
 	{
 		static void Main(string[] args)
 		{
-			Niko ()
-
+			Niko ();
 		}
 	}
 }
 		
-
-// Game State (Begin? End?)
-// Map, Player, Bad guys, obstacles, etc
-// For Map, you can use a grid. Decide where enemies and other players will go on map
-// 
